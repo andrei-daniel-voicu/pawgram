bfb5f0a75cd23c6cb7c9a74417d211ae505d8b2e
AlexandraPavel <72041403+AlexandraPavel@users.noreply.github.com>
2022-05-08 23:43:56 +0300

    publish branch

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(
-    "mongodb+srv://pawgram:parola@cluster0.dtj4g.mongodb.net/Pawgram",
-    //process.env.MONGO_URI,
-     {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('our db is connected');
-  })
-  .catch(err => console.log(err.message));
+// mongoose
+//   .connect(
+//     "mongodb+srv://pawgram:parola@cluster0.dtj4g.mongodb.net/Pawgram",
+//     //process.env.MONGO_URI,
+//      {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log('our db is connected');
+//   })
+//   .catch(err => console.log(err.message));
+
+const connect = async() => {
+  try{
+    await mongoose.connect(`mongodb+srv://pawgram:parola@cluster0.dtj4g.mongodb.net/Pawgram`, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+  } catch(error) {
+      console.log(error)
+  }
+}
+connect ()
