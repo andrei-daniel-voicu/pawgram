ce611aaa2f79f636e38595713197d16c4051dc17
Andrei-Daniel Voicu <saitatter133@gmail.com>
2022-05-30 02:17:00 +0300

    likes

--- frontend/app/components/UserProfile.js
+++ frontend/app/components/UserProfile.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
-    View, 
+    View,
     StyleSheet,
     Text,
     TouchableOpacity,
@@ -10,7 +10,7 @@ import {
     FlatList,
     Linking,
     Alert
- } from 'react-native';
+} from 'react-native';
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { useLogin } from '../context/LoginProvider';
 import { DrawerActions } from '@react-navigation/native';
@@ -27,11 +27,13 @@ const UserProfile = ({ navigation }) => {
         fetch(`http://localhost:2345/get-all-posts/${profile._id}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"}
+                "Content-Type": "application/json"
+            }
         })
             .then((response) => {
                 // console.log ("Response", response)
-                return response.json()})
+                return response.json()
+            })
             .then((responseJson) => {
                 setPosts(responseJson);
                 return responseJson;
@@ -42,11 +44,13 @@ const UserProfile = ({ navigation }) => {
         fetch(`http://localhost:2345/get-followers/${profile._id}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"}
+                "Content-Type": "application/json"
+            }
         })
             .then((response) => {
                 // console.log ("Response", response)
-                return response.json()})
+                return response.json()
+            })
             .then((responseJson) => {
                 setFollowers(responseJson);
                 return responseJson;
@@ -57,11 +61,13 @@ const UserProfile = ({ navigation }) => {
         fetch(`http://localhost:2345/get-following/${profile._id}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"}
+                "Content-Type": "application/json"
+            }
         })
             .then((response) => {
                 // console.log ("Response", response)
-                return response.json()})
+                return response.json()
+            })
             .then((responseJson) => {
                 setFollowing(responseJson);
                 return responseJson;
@@ -69,52 +75,85 @@ const UserProfile = ({ navigation }) => {
             .catch((error) => {
                 console.error(error);
             });
-      }, [profile]);
-    
-    const ItemView = ({item}) => {
+    }, [profile]);
+
+    const ItemView = ({ item }) => {
+        let icon_name = "thumbs-up-outline";
+
         return (
             <View style={styles.mediaImageContainer}>
                 <Text
-                style={styles.itemStyle}
+                    style={styles.itemStyle}
                 // onPress={() => getItem(item)}
                 >
-                {item["text"]}
+                    {item["text"]}
 
                 </Text>
-                <Image source={ 
+                <TouchableOpacity style={styles.buttonGPlusStyle} onPress={async () => {
+                    let found = false;
+                    for (let i = 0; i < item["likesList"].length; i++) {
+                        if (item["likesList"][i] === profile._id) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found == false) {
+
+                        const rest = await fetch(`http://localhost:2345/add-like/${item['_id']}`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                id: profile._id,
+                            })
+                        })
+                    }
+                    else {
+                        icon_name = "thumbs-up";
+                    }
+                    console.log(icon_name);
+                }}>
+                    <View style={styles.likeButton}
+                    >
+                        <Ionicons name={icon_name === "thumbs-up" ? "thumbs-up" : "thumbs-up-outline"} size={40} color="#DFD8C8"></Ionicons>
+                    </View>
+                </TouchableOpacity>
+
+                <Image source={
                     item["photoLink"]}
                     style={styles.image} resizeMode="cover"></Image>
-            </View>
+            </View >
         );
-      };
-     
-      const ItemSeparatorView = () => {
-        return (
-          // Flat List Item Separator
-          <View
-            style={{
-              height: 1,
-              width: '100%',
-              backgroundColor: '#C8C8C8',
-            }}
-          />
-        );
-      };
+    };
 
-      const handlePatreon = useCallback(async () => {
+    const ItemSeparatorView = () => {
+        return (
+            // Flat List Item Separator
+            <View
+                style={{
+                    height: 1,
+                    width: '100%',
+                    backgroundColor: '#C8C8C8',
+                }}
+            />
+        );
+    };
+
+    const handlePatreon = useCallback(async () => {
         // Checking if the link is supported for links with custom URL scheme.
         try {
             const supported = await Linking.canOpenURL(profile.patreonLink);
             if (!supported) {
                 Alert.alert(`Don't know how to open this URL: ${profile.patreonLink}`);
-            } 
-        } catch(e) {
+            }
+        } catch (e) {
             console.log(e);
         }
-      }, [profile.patreonLink]);
-    
+    }, [profile.patreonLink]);
+
     return (
-    <SafeAreaView style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={true}>
                 <View style={{ alignSelf: "center" }}>
                     <View style={styles.profileImage}>
@@ -139,37 +178,37 @@ const UserProfile = ({ navigation }) => {
                     padding: 20,
                     backgroundColor: '#f6f6f6',
                     marginTop: 20,
-                    marginHorizontal:100
+                    marginHorizontal: 100
                 }}>
                     <TouchableOpacity
-                            style={{
+                        style={{
                             backgroundColor: '#f6f6f6',
                             padding: 20,
-                            }}
-                        >
-                            <Text>{posts.length} Posts</Text>
+                        }}
+                    >
+                        <Text>{posts.length} Posts</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         style={{
-                        backgroundColor: '#f6f6f6',
-                        padding: 20,
+                            backgroundColor: '#f6f6f6',
+                            padding: 20,
                         }}
-                        // onPress={() => follow(false)}
+                    // onPress={() => follow(false)}
                     >
                         <Text>{followers.length} Followers</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         style={{
-                        backgroundColor: '#f6f6f6',
-                        padding: 20,
+                            backgroundColor: '#f6f6f6',
+                            padding: 20,
                         }}
-                        // onPress={() => setIsLoggedIn(false)}
+                    // onPress={() => setIsLoggedIn(false)}
                     >
                         <Text>{following.length} Following</Text>
                     </TouchableOpacity>
-                 </View>
+                </View>
 
-                <View style={{  alignSelf: "center", marginTop: 16 }}>
+                <View style={{ alignSelf: "center", marginTop: 16 }}>
                     <TouchableOpacity style={styles.buttonGPlusStyle}
                         onPress={() => navigation.dispatch(DrawerActions.jumpTo('Post'))}>
                         <View style={styles.add}>
@@ -196,7 +235,7 @@ const UserProfile = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
 
-                <View style={{ marginTop: 80, alignItems: "center", justifyContent: "center"}}>
+                <View style={{ marginTop: 80, alignItems: "center", justifyContent: "center" }}>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <FlatList
                             data={posts}
@@ -213,142 +252,145 @@ const UserProfile = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF"
-},
-text: {
-    fontFamily: "HelveticaNeue",
-    color: "#52575D"
-},
-image: {
-    flex: 1,
-    height: undefined,
-    width: undefined
-},
-titleBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 24,
-    marginHorizontal: 16
-},
-subText: {
-    fontSize: 12,
-    color: "#AEB5BC",
-    textTransform: "uppercase",
-    fontWeight: "500"
-},
-profileImage: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    overflow: "hidden"
-},
-add: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    right: 120,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center"
-},
-chat: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    bottom: 0,
-    top: 0,
-    right: 60,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center"
-},
-donate: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    bottom: 0,
-    top: 0,
-    left: 60,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center"
-},
-adopt: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    bottom: 0,
-    top: 0,
-    left: 120,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center"
-},
-infoContainer: {
-    alignSelf: "center",
-    alignItems: "center",
-    marginTop: 16
-},
-statsContainer: {
-    flexDirection: "row",
-    alignSelf: "center",
-    marginTop: 32
-},
-statsBox: {
-    alignItems: "center",
-    flex: 1
-},
-mediaImageContainer: {
-    width: 180,
-    height: 200,
-    borderRadius: 12,
-    overflow: "hidden",
-    marginHorizontal: 10
-},
-mediaCount: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: "50%",
-    marginTop: -50,
-    marginLeft: 30,
-    width: 100,
-    height: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 12,
-    shadowColor: "rgba(0, 0, 0, 0.38)",
-    shadowOffset: { width: 0, height: 10 },
-    shadowRadius: 20,
-    shadowOpacity: 1
-},
-recent: {
-    marginLeft: 78,
-    marginTop: 32,
-    marginBottom: 6,
-    fontSize: 10
-},
-recentItem: {
-    flexDirection: "row",
-    alignItems: "flex-start",
-    marginBottom: 16
-},
-activityIndicator: {
-    backgroundColor: "#CABFAB",
-    padding: 4,
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    marginTop: 3,
-    marginRight: 20 
-}
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF"
+    },
+    text: {
+        fontFamily: "HelveticaNeue",
+        color: "#52575D"
+    },
+    image: {
+        flex: 1,
+        height: undefined,
+        width: undefined
+    },
+    titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginHorizontal: 16
+    },
+    subText: {
+        fontSize: 12,
+        color: "#AEB5BC",
+        textTransform: "uppercase",
+        fontWeight: "500"
+    },
+    profileImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        overflow: "hidden"
+    },
+    add: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        right: 120,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    chat: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        top: 0,
+        right: 60,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    donate: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        top: 0,
+        left: 60,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    adopt: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        top: 0,
+        left: 120,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    infoContainer: {
+        alignSelf: "center",
+        alignItems: "center",
+        marginTop: 16
+    },
+    statsContainer: {
+        flexDirection: "row",
+        alignSelf: "center",
+        marginTop: 32
+    },
+    statsBox: {
+        alignItems: "center",
+        flex: 1
+    },
+    mediaImageContainer: {
+        width: 180,
+        height: 200,
+        borderRadius: 12,
+        overflow: "hidden",
+        marginHorizontal: 10
+    },
+    mediaCount: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: "50%",
+        marginTop: -50,
+        marginLeft: 30,
+        width: 100,
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        shadowColor: "rgba(0, 0, 0, 0.38)",
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 20,
+        shadowOpacity: 1
+    },
+    recent: {
+        marginLeft: 78,
+        marginTop: 32,
+        marginBottom: 6,
+        fontSize: 10
+    },
+    recentItem: {
+        flexDirection: "row",
+        alignItems: "flex-start",
+        marginBottom: 16
+    },
+    activityIndicator: {
+        backgroundColor: "#CABFAB",
+        padding: 4,
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        marginTop: 3,
+        marginRight: 20
+    },
+    likeButton: {
+        marginRight: 10
+    }
 });
 
 export default UserProfile;
